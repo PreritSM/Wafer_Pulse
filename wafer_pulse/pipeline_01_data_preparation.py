dc45e92f58eb8426cd3e3e57b3bb92a402b2a00b
PreritSM <prerit.mittal29@gmail.com>
2026-01-29 15:20:51 -0500

    Config reader Created

--- wafer_pulse/pipeline_01_data_preparation.py
+++ wafer_pulse/pipeline_01_data_preparation.py
@@ -4,13 +4,30 @@ import argparse
 import yaml
 import logging
 
+def read_params(config_path):
+    """
+    Reads a YAML configuration file and returns the parameters as a dictionary.
+    """
+    with open(config_path, 'r') as file:
+        params = yaml.safe_load(file)
+    return params
+
+def main(config_path,datasource_path):
+    """
+    Main function to prepare data based on the configuration file and data source path.
+    """
+    config = read_params(config_path)
+    print(f"Configuration parameters: {config}")
+
 
 if __name__ == "__main__":
     args = argparse.ArgumentParser()
+    default_config_path=os.path.join("config", "params.yaml")
     args.add_argument(
         "--config",
         type=str,
-        default="default.yaml",
+        
+        default=default_config_path,
         help="Path to the configuration file.",
     )
     args.add_argument(
@@ -22,3 +39,4 @@ if __name__ == "__main__":
 
     parsed_args = args.parse_args()
     print(f"Parsed arguments: {parsed_args}")
+    main(config_path=parsed_args.config, datasource_path=parsed_args.datasource)
